8adde2be91a9b92034f422f88e55aaa3fba5d571
gopikac2312-collab <gopikac2312@gmail.com>
2026-07-25 14:22:35 +0530

    add missing email utility

---
+++ src/utils/email.js
@@ -0,0 +1,26 @@
+const nodemailer = require("nodemailer");
+
+const transporter = nodemailer.createTransport({
+  host: process.env.SMTP_HOST,
+  port: process.env.SMTP_PORT,
+  secure: process.env.SMTP_PORT === "465",
+  auth: {
+    user: process.env.SMTP_USER,
+    pass: process.env.SMTP_PASS,
+  },
+});
+
+const sendResetPasswordEmail = async (to, resetUrl) => {
+  await transporter.sendMail({
+    from: process.env.SMTP_FROM || process.env.SMTP_USER,
+    to,
+    subject: "Reset your Qnayds password",
+    html: `
+      <p>You requested a password reset.</p>
+      <p><a href="${resetUrl}">Click here to reset your password</a></p>
+      <p>This link expires in 1 hour. If you didn't request this, you can ignore this email.</p>
+    `,
+  });
+};
+
+module.exports = { sendResetPasswordEmail };
