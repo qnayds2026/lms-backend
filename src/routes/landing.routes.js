8c070ff0aaedd5572c2cfb2fb3057a6dcfa2a9ec
Rohin Madhav <rohinmadhavk7@gmail.com>
2026-07-29 15:39:25 +0530

    resend mail for test

--- src/routes/landing.routes.js
+++ src/routes/landing.routes.js
@@ -1,7 +1,11 @@
 const router = require("express").Router();
 
-const { createLandingOrder } = require("../controllers/landing.controllers");
+const {
+  createLandingOrder,
+  resendActivationEmail,
+} = require("../controllers/landing.controllers");
 
 router.post("/create-order", createLandingOrder);
+router.post("/resend-activation", resendActivationEmail);
 
 module.exports = router;
