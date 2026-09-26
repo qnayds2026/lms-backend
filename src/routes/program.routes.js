d6fb4af0261f597e268932339c354d76df65d800
dilshad-mk <diludilshad1001@gmail.com>
2026-09-26 11:14:33 +0530

    role added to program route

--- src/routes/program.routes.js
+++ src/routes/program.routes.js
@@ -5,8 +5,8 @@ const registrationController = require("../controllers/programRegistration.contr
 const auth = require("../middleware/auth.middleware");
 const role = require("../middleware/role.middleware");
 
-// Create Program
-router.post("/", programController.create);
+// Admin: Create Program
+router.post("/", auth, role("ADMIN"), programController.create);
 
 // Get All Programs
 router.get("/", programController.getAll);
@@ -14,14 +14,14 @@ router.get("/", programController.getAll);
 // Get Single Program
 router.get("/:id", programController.getOne);
 
-// Update Program
-router.patch("/:id", programController.update);
+// Admin: Update Program
+router.patch("/:id", auth, role("ADMIN"), programController.update);
 
-// Activate Program
-router.patch("/:id/activate", programController.activate);
+// Admin: Activate Program
+router.patch("/:id/activate", auth, role("ADMIN"), programController.activate);
 
-// Deactivate Program
-router.patch("/:id/deactivate", programController.deactivate);
+// Admin: Deactivate Program
+router.patch("/:id/deactivate", auth, role("ADMIN"), programController.deactivate);
 
 // Public: Register for a Program (external landing page)
 router.post("/:programId/register", registrationController.register);
